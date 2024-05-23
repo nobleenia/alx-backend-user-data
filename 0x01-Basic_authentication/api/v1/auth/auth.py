693603cac0a13adafc8aa69f2a43b4a90c201780
nobleenia <neluwah@gmail.com>
2024-05-23 07:51:13 +0000

    Corrections: api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,6 +6,7 @@ from flask import request
 from typing import List, TypeVar
 import fnmatch
 
+
 class Auth:
     """
     Auth class to manage the API authentication
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -97,16 +97,14 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-
-        user_list = User.search({"email": user_email})
-        if not user_list:
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
             return None
-
-        user = user_list[0]
-        if not user.is_valid_password(user_pwd):
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
             return None
-
-        return user
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
